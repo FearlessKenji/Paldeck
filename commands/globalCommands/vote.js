996837c9b1e7aaec24d9bbafca91139408ebf718
FearlessKenji <160550099+FearlessKenji@users.noreply.github.com>
2024-04-07 03:26:29 -0400

    Create vote.js

---
+++ commands/globalCommands/vote.js
@@ -0,0 +1,16 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('vote')
+		.setDescription('Vote for Paldeck.'),
+	async execute(interaction) {
+		try {
+			await interaction.reply({ content: 'https://top.gg/bot/1218928129628966963/vote' });
+		}
+		catch (error) {
+			console.error('Failed to update server settings:', error);
+			await interaction.reply({ content: 'Failed to generate vote link.', ephemeral: true });
+		}
+	},
+};
