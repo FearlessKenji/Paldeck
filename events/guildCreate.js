3bf2c0a6ee13c83322298afc4b66b46603475ae3
FearlessKenji <160550099+FearlessKenji@users.noreply.github.com>
2024-04-13 00:12:54 -0400

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -5,7 +5,8 @@ module.exports = {
 	name: Events.GuildCreate,
 	async execute(guild) {
 		try {
-			console.log(writeLog(`Added to new server: ${guild.name} ID: ${guild.id}.`));
+			const owner = await guild.fetchOwner();
+			console.log(writeLog(`Added to new server: ${guild.name}) | ID: ${guild.id}\nOwner: ${owner} | OwnerUsername: ${owner.user.username}.`));
 		}
 		catch (error) {
 			console.error(writeLog('Failed to update server table upon arrival.', error));
